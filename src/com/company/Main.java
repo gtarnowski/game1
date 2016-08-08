2e64209d5c014fc79c04d77d90c946a701765078
gtarnowski <g.tarnowski@proinserv.pl>
2016-08-08 10:00:38 +0200

    poprawka class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,36 @@
 package com.company;
 
+import java.util.Scanner;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
+
 public class Main {
 
-    public static void main(String[] args) {
-        Engine engine = new Engine();
-        engine.gameMenu();
+    NewPlayer player = new NewPlayer();
+    public void main(String[] args) {
+        gameMenu();
+    }
 
+    public void gameMenu() {
+        Scanner sc = new Scanner(in);
+        int result;
+
+        out.println("1. New Game");
+        out.println("2. Exit");
+        result = sc.nextInt();
+
+        if (result == 1) {
+            player.newGame();
+        } else if (result == 2) {
+            out.println("Game ending");
+            out.println(".");
+            out.println("..");
+            out.println("...");
+            System.exit(0);
+        } else {
+            out.println("Wrong choose, try again");
+            gameMenu();
+        }
     }
 }
--- src/com/company/NewPlayer.java
+++ src/com/company/NewPlayer.java
@@ -9,14 +9,14 @@ import static java.lang.System.out;
 
 public class NewPlayer extends Player {
     Player player = new Player();
-
+    Engine engine = new Engine();
     public void newGame(){
         buildIdentity();
         buildCharacter();
-        calculateStatByStrenght();
-        calculateStatByAgility();
-        calculateStatByVitality();
-        calculateStatByEnergy();
+        engine.calculateStatByStrenght();
+        engine.calculateStatByAgility();
+        engine.calculateStatByVitality();
+        engine.calculateStatByEnergy();
     }
     private void buildIdentity(){
         Scanner sc = new Scanner(in);
@@ -54,78 +54,6 @@ public class NewPlayer extends Player {
             //Arch stats
             player.hp = 80; player.mp = 30;
             player.str = 22; player.agi = 25; player.vit = 20;player.ene = 15;
-        }
-    }
-
-    //musi byc odrebna metoda
-    private void calculateStatByStrenght(){
-        //TODO: wyliczyc odrebne staty
-        //SM attack set -
-        if(Objects.equals(this.player.selectedClass, profession[0])){
-            player.attack[0] = (byte) (this.player.str /4);
-            player.attack[1] = (byte) (this.player.str /6);
-        }
-
-        //DK attack set - staty poprawne
-        if(Objects.equals(this.player.selectedClass, profession[1])){
-            player.attack[0] = (byte) (this.player.str /4);
-            player.attack[1] = (byte) (this.player.str /6);
-        }
-        //TODO: wyliczyc odrebne staty
-        //ARCH attack set -
-        if(Objects.equals(this.player.selectedClass, profession[2])){
-            player.attack[0] = (byte) (this.player.str /4);
-            player.attack[1] = (byte) (this.player.str /6);
-        }
-    }
-    private void calculateStatByAgility(){
-        //TODO: wyliczyc odrebne staty
-        //SM attack set -
-        if(Objects.equals(this.player.selectedClass, profession[0])){
-            player.def = (byte) (this.player.agi /4);
-        }
-
-        //DK attack set -
-        if(Objects.equals(this.player.selectedClass, profession[1])){
-            player.def = (byte) (this.player.agi /3);
-        }
-        //ARCH attack set -
-        if(Objects.equals(this.player.selectedClass, profession[2])){
-            player.def = (byte) (this.player.agi /4);
-        }
-    }
-    private void calculateStatByVitality(){
-        //TODO: wyliczyc odrebne staty
-        //SM vit
-        if(Objects.equals(this.player.selectedClass, profession[0])){
-            player.hp= (byte) (30 + (this.player.lvl -1 ) + this.player.vit * 2);
-        }
-
-        //DK vit
-        //HP = 35+(lvl-1)*2+vit*3
-        if(Objects.equals(this.player.selectedClass, profession[1])){
-            player.hp= (byte) (35 + (player.lvl - 1)*2 + player.vit * 3);
-        }
-        //ARCH vit
-        //HP = 40+(lvl-1)+vit*2
-        if(Objects.equals(this.player.selectedClass, profession[2])){
-            player.hp= (byte) (40+ (player.lvl - 1) + player.vit * 2);
-        }
-    }
-    private void calculateStatByEnergy(){
-        //SM ene + mp calc
-        if(Objects.equals(this.player.selectedClass, profession[0])){
-            player.attackMagic[0]= (byte) (this.player.ene/4);
-            player.attackMagic[1]= (byte) (this.player.ene/9);
-            player.mp= (byte) ((this.player.lvl - 1) * 2 + this.player.ene * 2);
-        }
-        //DK ene calc
-        if(Objects.equals(this.player.selectedClass, profession[1])){
-            player.mp= (byte) (10 + (this.player.lvl -1) * 0.5 + this.player.ene);
-        }
-        //ARCH ene calc
-        if(Objects.equals(this.player.selectedClass, profession[2])){
-            player.mp = (byte) (6 + (this.player.lvl * 1.5) + (this.player.ene * 1.5));
         }
     }
 }
